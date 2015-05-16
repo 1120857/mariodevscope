5bd3346145f4751b97e01e3cefe91b0b97b51000
mariodevscope <1120857@isep.ipp.pt>
2015-05-16 16:01:38 +0100

    initial revision

---
+++ mariodevscope/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mariodevscope.MvcApplication" Language="C#" %>
---
+++ mariodevscope/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mariodevscope.Startup))]
+namespace mariodevscope
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
